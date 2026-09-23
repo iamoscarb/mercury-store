fedce0d84a977122405bcfe3ecbaca820778e668
Oscar Bazan <oscaribh97@gmail.com>
2026-09-23 17:32:41 -0600

    adds shadcn lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "./components/ui/button"
+
 function App() {
 
   return (
     <>
       <p className='text-3xl font-bold underline'>Hello World!</p>
+      <Button>Botón Shadcn</Button>
     </>
   )
 }
